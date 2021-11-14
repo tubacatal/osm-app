3d0185a9837ee75a6c5360dc33673da88d4ced17
tubacatal <tubacatal90@gmail.com>
2021-11-14 21:49:36 +0300

    zoom level changed for better centralizing

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,7 +7,7 @@ import './map.css';
 
 const Map = () => {
   return (
-    <MapContainer center={BERLIN} zoom={13} scrollWheelZoom={false} className="map">
+    <MapContainer center={BERLIN} zoom={11} scrollWheelZoom={false} className="map">
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

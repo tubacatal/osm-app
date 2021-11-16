9fca2d3c99ada3ffe87633a3c26a99d0f9bc4b7b
tubacatal <tubacatal90@gmail.com>
2021-11-16 19:56:56 +0300

    geojson display implemented. unused variables removed.

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,33 +1,43 @@
+import L from 'leaflet';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { MapContainer, FeatureGroup, TileLayer } from 'react-leaflet';
+import { useDispatch, useSelector } from 'react-redux';
+import { MapContainer, FeatureGroup, TileLayer, GeoJSON } from 'react-leaflet';
 import { EditControl } from 'react-leaflet-draw';
 import { OSM, DRAW_OPTIONS } from '../../assets/consts';
 import { addShape } from '../../store/map/action';
-
 import MarkerComponent from './marker/marker';
-
+import { fetchBoundingBox, convertOsmToGeoJson } from "../../services/osm-services";
+import icon from '../../assets/images/marker-icon.png';
+import iconRetina from '../../assets/images/marker-icon-2x.png';
+import iconShadow from '../../assets/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import './map.css';
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
 const Map = () => {
   const dispatch = useDispatch();
+  const data = useSelector(state => state.map.shapes);
 
-  const handleCreated = (e) => {
-    if (e.layerType === 'rectangle') {
-      // get bbox from rectangle
-      const bounds = e.layer.getBounds();
-      dispatch(addShape({
-        shape: e,
-        bounds: {
-          'northWest': bounds.getNorthWest(),
-          'northEast': bounds.getNorthEast(),
-          'southEast': bounds.getSouthEast(),
-          'southWest': bounds.getSouthWest(),
-        }
-      }));
-    }
+  const handleCreated = async (e) => {
+    const bounds = e.layer.getBounds();
+    // get bbox from the bounds
+    const bbox = bounds.toBBoxString();
+    const bboxData = bbox?.split(',');
+    // api call to fetch osm data for bbox, convert it to geojson
+    const bBoxOsmData = await fetchBoundingBox(bboxData[0], bboxData[1], bboxData[2], bboxData[3]);
+    const geoJSONData = convertOsmToGeoJson(bBoxOsmData);
+    dispatch(addShape({
+      center: bounds.getCenter(),
+      geoJSON: geoJSONData,
+    }));
   };
 
   const handleEdited = (e) => {
@@ -44,7 +54,12 @@ const Map = () => {
           draw={DRAW_OPTIONS}
         />
       </FeatureGroup>
-      <MarkerComponent position={OSM.center} />
+      {data?.map((shape, index) => 
+        <div key={index}>
+          <GeoJSON data={shape?.geoJSON} />
+          <MarkerComponent position={[shape?.center?.lat, shape?.center?.lng]} />
+        </div>
+      )}
       <TileLayer attribution={OSM.attribution} url={OSM.url} />
     </MapContainer>
   );
